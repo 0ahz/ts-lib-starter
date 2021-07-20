eb067c2908e617b1861086275294fe29ca52aae3
0ahz <shuangoahz@gmail.com>
2021-07-20 15:00:42 +0800

    chore: up

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  transform: {
-    '^.+\\.tsx?$': '@sucrase/jest-plugin',
-  },
   testPathIgnorePatterns: ['/node_modules/', '/dist/', '/types/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 }
