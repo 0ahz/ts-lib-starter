d9d3ed2dca368a9b3cc9cc470480777be1ccb7ab
0ahz <shuangoahz@gmail.com>
2022-07-08 13:43:49 +0800

    chore: up

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,18 +1,21 @@
+import camelCase from 'camelcase'
 import { defineConfig } from 'tsup'
+
+import pkg from './package.json'
 
 // https://tsup.egoist.sh/#using-custom-configuration
 // https://paka.dev/npm/tsup/v/5.12.4#module-index-export-Options
 export default defineConfig(() => {
   return {
     // platform: 'node',
-    // target: ['node12'],
+    // target: ['node16'],
     platform: 'browser',
-    target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
+    target: ['chrome60', 'firefox60', 'safari11', 'edge18'],
     entry: {
       index: 'src/index.ts',
     },
     outdir: 'dist',
-    globalName: 'MyLibName',
+    globalName: camelCase(pkg.name),
     format: ['cjs', 'esm', 'iife'],
     bundle: true,
     splitting: true,
@@ -26,5 +29,6 @@ export default defineConfig(() => {
       // https://esbuild.github.io/api/#simple-options
       console.log(options, context)
     },
+    onSuccess: `echo onSuccess`,
   }
 })
