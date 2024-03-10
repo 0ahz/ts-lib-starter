427e27c911e1c87040e7f7de55bf2fff0aff637a
0ahz <shuangoahz@gmail.com>
2024-03-10 12:24:25 +0800

    chore: up

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,34 +1,23 @@
 import { defineConfig } from 'tsup'
 
-// https://tsup.egoist.sh/#using-custom-configuration
-// https://paka.dev/npm/tsup/v/5.12.4#module-index-export-Options
 export default defineConfig(options => {
   return {
     // platform: 'node',
     // target: ['node16'],
     platform: 'browser',
-    target: ['chrome60', 'firefox60', 'safari11', 'edge18'],
+    target: ['es2020', 'chrome60', 'firefox60', 'safari11', 'edge18'],
     entry: {
       index: 'src/index.ts',
       'sub/index': 'src/sub/index.ts',
     },
     outDir: 'dist',
-    format: ['esm', 'cjs'],
-    dts: {
-      resolve: true,
-    },
+    format: ['cjs', 'esm'],
+    dts: true,
     bundle: true,
     splitting: true,
     sourcemap: false,
     treeshake: true,
     clean: true,
     minify: !options.watch,
-
-    esbuildPlugins: [],
-
-    // esbuildOptions(options, context) {
-    //   // https://esbuild.github.io/api/#simple-options
-    //   console.log(options, context)
-    // },
   }
 })
