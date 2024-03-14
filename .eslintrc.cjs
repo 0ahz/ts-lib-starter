5032469524c4c8be29fa6c5214a440d46658a722
0ahz <shuangoahz@gmail.com>
2024-03-14 13:09:02 +0800

    chore: up

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,5 +14,18 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
   ],
   plugins: [],
-  rules: {},
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'all',
+        argsIgnorePattern: '^_',
+        caughtErrors: 'all',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
+  },
 }
