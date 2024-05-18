a8a1d1b12a76bde29f33f9c77dbfae3ea7bc71ea
0ahz <shuangoahz@gmail.com>
2024-05-18 19:01:16 +0800

    chore: up eslint

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import unjs from 'eslint-config-unjs'
+
+export default unjs(
+  {
+    ignores: [
+      'dist',
+      'node_modules',
+      //
+    ],
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+)
